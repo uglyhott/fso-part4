bca89dad79fea81365f67ec9fe8c2473cc659074
Liam Blumenstein-Tait <liam.bt@gmail.com>
2024-09-30 21:53:06 +0200

    4.4: Helper Functions and Unit Tests, step 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,4 +2,11 @@ const dummy = (blogs) => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  return blogs.reduce((acc, blog) => acc + blog.likes, 0)
+}
+
+module.exports = {
+  dummy,
+  totalLikes
+}
