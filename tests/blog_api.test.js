b1f75ac5fc0b34d6e35b9146f51c27e20eaeccc7
Liam Blumenstein-Tait <liam.bt@gmail.com>
2024-10-12 21:00:59 +0200

    4.9: Blog List Tests, step 2

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -21,9 +21,17 @@ describe.only('with initial Blogs already saved', () => {
     await Promise.all(promiseArray);
   });
 
-  it.only('all blogs are returned', async () => {
+  it('all blogs are returned', async () => {
     const response = await api.get('/api/blogs');
     assert.strictEqual(response.body.length, helper.initialBlogs.length);
+  });
+
+  it.only('blog object uses id and not _id', async () => {
+    const blogs = await helper.blogsInDb();
+    const checkId = Object.keys(blogs[0]).includes('id');
+    const checkUnderscoreId = Object.keys(blogs[0]).includes('_id');
+    assert.strictEqual(checkId, true, 'id key not found');
+    assert.strictEqual(checkUnderscoreId, false, '_id key found');
   });
 });
 
